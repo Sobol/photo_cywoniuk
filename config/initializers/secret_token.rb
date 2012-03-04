15b5130962adb0b2f459103b1cd6fc61b2d678c8
Paweł Sobolewski <pawel.sobolewski@amberbit.com>
2012-03-04 16:06:54 +0100

    Inmitialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PhotoCywoniuk::Application.config.secret_token = '6e352da0fc1871601c8cf387b45bc37b3e18639484c46b269453a632f2f21fc05590fef758503838ab17ce9d8639528371a5d351a3fb7aeaed8e406390ee8906'
