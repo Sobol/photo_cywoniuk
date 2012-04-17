dc3289104bd83d697277b70aa7a81feb3b4be853
Paweł Sobolewski <pawel.sobolewski@amberbit.com>
2012-04-17 18:16:58 +0200

    Paperclip -> amazon s3

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -3,7 +3,14 @@ class Gallery < ActiveRecord::Base
     
   has_many :gallery_images, :dependent => :destroy, :class_name => "GalleryImage", :foreign_key => "gallery_id"
 
-  has_attached_file :photo, 
+  has_attached_file :photo,
+                    :storage => :s3,
+                    :s3_credentials => {:access_key_id => ENV['S3_KEY'],
+                                        :secret_access_key => ENV['S3_SECRET']},
+                    :bucket => "photocywoniuk",
+                    :path => ":class/:id/:basename/:style.:extension",
+                    :s3_headers => { :cache_control => "max-age=#{364.days.to_i}" },
+                    :s3_permissions => :public_read,
                     :styles => { 
                       :medium => "300x200#" }
 end
--- app/models/gallery_image.rb
+++ app/models/gallery_image.rb
@@ -6,7 +6,14 @@ class GalleryImage < ActiveRecord::Base
   
   belongs_to :gallery, :class_name => "Gallery"
   
-  has_attached_file :image, 
+  has_attached_file :image,
+                    :storage => :s3,
+                    :s3_credentials => {:access_key_id => ENV['S3_KEY'],
+                                        :secret_access_key => ENV['S3_SECRET']},
+                    :bucket => "photocywoniuk",
+                    :path => ":class/:id/:basename/:style.:extension",
+                    :s3_headers => { :cache_control => "max-age=#{364.days.to_i}" },
+                    :s3_permissions => :public_read,
                     :styles => { 
                       :thumb => "114x72#",
                       :medium => "920x360#" }
